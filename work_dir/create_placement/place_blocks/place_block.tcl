d83bf00358341aee9c99c2a184851d071fd82554
DillibabuShanmugam <90065623+DillibabuShanmugam@users.noreply.github.com>
2023-05-10 00:24:24 -0400

    Add files via upload

---
+++ work_dir/create_placement/place_blocks/place_block.tcl
@@ -0,0 +1,18 @@
+# Product: R-2020.09-SP3 Date: Jan 19, 2021
+send_status -status "Worker_msg: $block_refname in placement."
+if { [get_attribute -name open_mode [current_block]] == "read" } {
+  reopen_block [current_block]
+}
+set restore_fix_hard_macro false
+if {[get_app_option_value -name place.coarse.fix_hard_macros -block [current_block]]==false} {
+    set restore_fix_hard_macro true
+    set_app_options -block [current_block] -list {place.coarse.fix_hard_macros true}
+}
+create_placement -floorplan -congestion  -congestion_effort medium -sub_block
+if {$restore_fix_hard_macro==true} {
+  reset_app_options -block [current_block] place.coarse.fix_hard_macros
+  if {[get_app_option_value -name place.coarse.fix_hard_macros -block [current_block]]==true} {
+    set_app_options -block [current_block] -list {place.coarse.fix_hard_macros false}
+  }
+}
+set_app_options -list {plan.place.place_inside_blocks true}
